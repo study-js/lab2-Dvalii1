4eef1be82c10db6d8bac9880a99f3f5e95032223
Dvalii1 <dvalidito1@gmail.com>
2017-11-08 22:35:03 +0400

    L204

---
+++ L204/script.js
@@ -0,0 +1,10 @@
+var password = "paroli";
+var sheyvana = prompt("შეიყვანეთ პაროლი: ");
+
+while (sheyvana !== "paroli") {
+
+    sheyvana = prompt("შეიყვანეთ პაროლი: ");
+
+}
+
+document.write("თქვენ შეხვედით!");
